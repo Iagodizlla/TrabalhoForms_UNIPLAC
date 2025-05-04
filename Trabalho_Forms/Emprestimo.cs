913cba1fd6d4026c469c180c7198fb1a6a9a4431
Iagodizlla <iagoschlemper@gmail.com>
2025-05-03 22:34:13 -0300

    Implemetado construtor de emprestimo

--- Trabalho_Forms/Emprestimo.cs
+++ Trabalho_Forms/Emprestimo.cs
@@ -7,4 +7,13 @@ public class Emprestimo
     public Exemplar Item { get; set; }
     public DateTime DataEmprestimo { get; set; }
     public DateTime DataDevolucao { get; set; }
+
+    public Emprestimo(Leitor origem, Leitor destino, Exemplar item, DateTime dataEmprestimo, DateTime dataDevolucao)
+    {
+        Origem = origem;
+        Destino = destino;
+        Item = item;
+        DataEmprestimo = dataEmprestimo;
+        DataDevolucao = dataDevolucao;
+    }
 }

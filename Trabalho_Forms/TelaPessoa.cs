d216fde18c84745c204b952cc928acef2f503504
Iagodizlla <iagoschlemper@gmail.com>
2025-06-01 20:25:17 -0300

    Retirado Enum desnecessario

--- Trabalho_Forms/TelaPessoa.cs
+++ Trabalho_Forms/TelaPessoa.cs
@@ -21,7 +21,6 @@ public partial class TelaPessoa : Form
         cargoFuncionario.DataSource = Enum.GetValues(typeof(EnumFuncionarioCargo));
         cargoFuncionario.SelectedIndex = 0;
         // carrega os valores do Enum para o combobox
-        listBoxTipo.DataSource = Enum.GetValues(typeof(EnumGenericoTipo));
         listBoxTipo.SelectedIndex = 0;
     }
 
